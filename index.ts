4362669f1f85132bed6deb239f837e6c079380ab
BenoitSerrano <benoit.serrano10@gmail.com>
2023-05-22 16:22:39 +0100

    add dotenv handling

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import Express from 'express';
 import cors from 'cors';
 import { Response } from 'express';
 import path from 'path';
+import { config } from './src/config';
 
 const app = Express();
 
@@ -13,6 +14,6 @@ app.get('/*', (_, res: Response) => {
     res.sendFile(path.join(__dirname, '..', 'src', 'client', 'build', 'index.html'));
 });
 
-app.listen(3000, async () => {
-    console.log(`Server is running on port 3000`);
+app.listen(config.PORT, async () => {
+    console.log(`Server is running on port ${config.PORT}`);
 });
